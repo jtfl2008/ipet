663a7962323ec910102c4e864bd60a64ed3401e5
dongjianglong <>
2022-10-15 11:48:37 +0800

    init🎉: 💐

---
+++ src/pages/index/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import './index.scss';
+let msg = ref('Hello taro');
+</script>
